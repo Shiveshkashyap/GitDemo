073001a393c4c8b92348330ed76465db49606bce
shiveshkashyap <shiv99esh@gmail.com>
2024-05-12 12:45:18 +0530

    this is 2nd commit on different time

---
+++ src/main/java/GitTest/GitTest1.java
@@ -0,0 +1,12 @@
+package GitTest;
+
+public class GitTest1 {
+
+    public static void main(String []args){
+        System.out.println("This has to be update in Git as well");
+        System.out.println("As I m the user who is updating it and push this into GIT");
+        System.out.println("So the next person will come and take the latest changes which i made");
+    }
+}
+
+
